b1af7f77052f3ae4096a13517c60a1c7564e14b1
Bryan Novak <bjnovak125@gmail.com>
2015-10-21 23:22:14 -0500

    updating style and rows/columns

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var index = express.Router();
 
 index.get('/', function (req, res) {
-	res.render('index', {theme:'flatly', title:'Bryan J Novak', h1:'B.J. Novak ', h1Small:'software developer'});
+	res.render('index', {theme:'flatly', title:'Bryan J Novak', h1:'Bryan J Novak ', h1Small:'software developer'});
 });
 
 module.exports = index;
